4b3595b1ad430221d06dee45c13e822becbdba43
Ben <benjaminge.franklin@gmail.com>
2019-01-27 19:25:46 +0000

    HTTPS Support & GNU Terry Pratchett

--- app.js
+++ app.js
@@ -21,20 +21,26 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// GNU
+app.use(function (req, res, next) {
+    res.set('X-Clacks-Overhead', 'GNU Terry Pratchett');
+    next();
+});
+
 // catch 404 and forward to error handler
 //app.use(function(req, res, next) {
-  //next(createError(404));
+    //next(createError(404));
 //});
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 // DB stuff
@@ -48,19 +54,18 @@ var Post = mongoose.model('Post', postSchema);
 
 /* GET home page. */
 app.get('/', function(req, res) {
-  Post.find({}, function(err, posts) {
-    res.render('index', { title: 'Bloog', posts: posts });
-  });
+    Post.find({}, function(err, posts) {
+        res.render('index', { title: 'Bloog', posts: posts });
+    });
 });
 
 app.post('/addpost', function(req, res) {
-  var postData = new Post(req.body);
-  postData.save().then(function(result){
-    res.redirect('/');
-  }).catch(function(err){
-    res.status(400).send("Unable to save le data");
-  });
+    var postData = new Post(req.body);
+    postData.save().then(function(result){
+        res.redirect('/');
+    }).catch(function(err){
+        res.status(400).send("Unable to save le data");
+    });
 });
-
 
 module.exports = app;
